e8fc6d93e24c062bd886d49cf055dcad3fc90204
井古田 光晴 <ikotan.69@gmail.com>
2016-10-11 00:10:19 +0900

    angular2 sample

---
+++ app/components/footer/footer.component.ts
@@ -0,0 +1,19 @@
+import {Component} from "@angular/core";
+import {TodoService} from "../../services/todo.service";
+import {Todo} from "../../models/todo.model";
+
+@Component({
+  selector: 'todo-footer',
+  templateUrl: 'app/components/footer/footer.html'
+})
+export class TodoFooterComponent {
+
+  // @Input()
+  todos: Todo[];
+
+  constructor(private service: TodoService) {}
+
+  getCompletedCount() {
+    return this.service.getCompletedCount();
+  }
+}
